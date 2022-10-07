45b99b72c105dcb9d9043036d9a294bc944542f2
Nathan Cameron <62361875+cameronnathan707@users.noreply.github.com>
2022-10-07 08:39:25 -0600

    fix: display and chapter count

--- frontend/src/components/LocationTree/LocationTree.tsx
+++ frontend/src/components/LocationTree/LocationTree.tsx
@@ -24,37 +24,31 @@ export const LocationTree = ({ currentNode, tree }: IProps = {}) => {
       * Highlight the current node
       * Display the location count on all nodes expect location nodes 
       */}
-      {tree?.nodes?.map(program =>
+      <p className='program-heading'>
+        {/* <FontAwesomeIcon icon="globe" /> */}
+        {console.log(currentNode)}
+        {tree?.name}
+        <span className='chapter-count'> {findChapterCount(tree)}</span>
+      </p>
+      {tree?.nodes?.map(region =>
         {
           return (
           <>
-            <p className='program-heading'>
+            <p className='region-heading'>
               {/* <FontAwesomeIcon icon="globe" /> */}
-              {program.name}
-              <span className='chapter-count'> {findChapterCount(program)}</span>
+              {region.name}
+              <span className='chapter-count'> {findChapterCount(region)}</span>
             </p>
               {
-                program?.nodes?.map(region =>
+                region?.nodes?.map(chapter =>
                   {
                     return (
                     <>
-                      <p className='region-heading'>
+                      <p className='chapter-heading'>
                         {/* <FontAwesomeIcon icon="globe" /> */}
-                        {region.name}
-                        <span> {findChapterCount(region)}</span>
+                        {chapter.name}
+                        <span> {findChapterCount(chapter)}</span>
                       </p>
-                      {
-                          region?.nodes?.map(chapter =>
-                            {
-                              return (
-                                <p className='chapter-heading'>
-                                  {/* <FontAwesomeIcon icon="globe" /> */}
-                                  {chapter.name}
-                                </p>
-                              )
-                            }
-                          )
-                      }
                     </>
                     )
                   }
--- frontend/src/constants/find-chapter-count.ts
+++ frontend/src/constants/find-chapter-count.ts
@@ -1,6 +1,6 @@
 import { ProgramNode, ProgramNodeTypeEnum } from "../interfaces/program-node";
 
-export const findChapterCount = (node: ProgramNode) => {
+export const findChapterCount = (node?: ProgramNode) => {
   /* 
     TODO: Count how many chapter nodes within a program location node
 
@@ -8,12 +8,17 @@ export const findChapterCount = (node: ProgramNode) => {
     * If the node is a `chapter` it shouldn't count the nodes below
     * Count all the nodes within the tree with type of `chapter` within the program tree
   */
-  if (node.type === ProgramNodeTypeEnum.program) {
-    // @ts-ignore
-    
-    return node.nodes?.reduce((chapterCount: number, regions: ProgramNode) => chapterCount + regions?.nodes.length, 0);
+  if (node?.type === ProgramNodeTypeEnum.program) {
+    return node.nodes?.reduce((chapterCount: number, regions: ProgramNode) => {
+      if (regions?.nodes?.length) {
+        return chapterCount + regions?.nodes?.length;
+      } else {
+        return chapterCount;
+      }},
+      0
+    );
   }
-  else if (node.type === ProgramNodeTypeEnum.region) {
+  else if (node?.type === ProgramNodeTypeEnum.region) {
    return node.nodes?.length;
   } else {
     return;
